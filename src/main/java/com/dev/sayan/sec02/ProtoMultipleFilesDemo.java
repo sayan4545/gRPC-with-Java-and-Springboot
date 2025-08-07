f88fa3d7b7dfa3e08a8738155c322036b703ce7c
sayan4545 <sayanchatterjee3010@gmail.com>
2025-08-07 21:37:19 +0530

    Comparing two person object created with .equals and ==

--- src/main/java/com/dev/sayan/sec02/ProtoMultipleFilesDemo.java
+++ src/main/java/com/dev/sayan/sec02/ProtoMultipleFilesDemo.java
@@ -8,14 +8,26 @@ public class ProtoMultipleFilesDemo {
     private static final Logger log
             = LoggerFactory.getLogger(ProtoMultipleFilesDemo.class);
 
+    public static Person createPerson(){
+        return Person.newBuilder()
+                .setName("Sayan").setAge(31)
+                .build();
+    }
+
     public static void main(String[] args) {
 
-        var person = Person.newBuilder()
-                .setName("Sayan")
-                .setAge(27)
-                .build();
 
-        log.info("{}",person);
+        var person1 = createPerson();
+        var person2 = createPerson();
+        log.info("comparing with person1.equals(person2) {}",person1.equals(person2));
+        log.info("comparing person1==person2 {}",person1==person2);
+
+//        var person = Person.newBuilder()
+//                .setName("Sayan")
+//                .setAge(27)
+//                .build();
+//
+//        log.info("{}",person);
 
     }
 }
